ff5d25e367b9ebccd1082120989465604d4cf417
lukaslipe <lucascristodev@gmail.com>
2026-06-02 15:52:39 -0300

    Fix: Conflitos

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    {/* 2. Envolva o componente App com o BrowserRouter */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
