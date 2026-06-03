dbb82ccd1eee561d8b0b45f725aeb534520a9d47
Lucas Cristo <lucascristodev@gmail.com>
2026-06-03 18:51:40 -0300

    Retirando arquivos nao utilizados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import SetorPage from './components/Pages/Setor/SetorPage';
 import ServicoPage from './components/Pages/Servico/ServicoPage';
 import UsuarioPage from './components/Pages/Usuario/UsuarioPage';
 import ChamadoPage from './components/Pages/Chamado/ChamadoPage';
-import MensagemChamadoPage from './components/Pages/MensagemChamado/MensagemChamadoPage';
 import MainLayout from './components/Layout/MainLayout';
 import ProtectedRoute from './auth/ProtectedRoute';
 import { Toaster } from 'react-hot-toast';
@@ -17,19 +16,19 @@ function App() {
   return (
     <>
     <Toaster
-          position="top-right"
-          toastOptions={{
-              duration: 3000,
-          }}
+      position="top-right"
+      toastOptions={{
+          duration: 3000,
+      }}
     />
     <Routes>
 
       <Route path="/login" element={<LoginPage />} />
 
       <Route  element={
-          <ProtectedRoute>
-              <MainLayout />
-          </ProtectedRoute>
+        <ProtectedRoute>
+          <MainLayout />
+        </ProtectedRoute>
       }>
         <Route path="/setores" element={<SetorPage />} />
         <Route path="/servicos" element={<ServicoPage />} />
@@ -41,7 +40,6 @@ function App() {
                 </AdminRoute>
             }
         />
-        <Route path="/mensagens" element={<MensagemChamadoPage />} />
         <Route path="/novoChamado" element={<ChamadoPage />} />
         <Route path="/chamados/:id/editar" element={<ChamadoPage />}/>
         <Route path="/chamados" element={<ListaChamadosPage />} />
